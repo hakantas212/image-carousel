f154f4987ff9df9c62612b4a8b8ebd4e0925025e
hakantas212 <hakantas212@gmail.com>
2020-07-22 11:35:43 +0200

    index/translateX

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,46 +1,109 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
-import Images from "./Images";
 
 const Container = styled.div`
-  border: 1px solid orange;
+  border: 5px solid orange;
   position: relative;
-  height: 100vh;
-  width: 100vw;
-  margin: 0 auto;
   overflow: hidden;
+  width: 750px;
+  margin: 0 auto;
+  height: 735px;
+  white-space: nowrap;
+  button {
+    border: 1px solid orange;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 10%;
+  }
 `;
 const PrevButton = styled.button`
-  border: 1px solid orange;
-  display: flex;
-  position: absolute;
-  top: 50%;
+  left: 0;
 `;
 const NextButton = styled.button`
-  border: 1px solid orange;
-  position: absolute;
-  top: 50%;
-  right: 0px;
+  right: 0;
 `;
 
-const ImageSlider = (props) => {
-  const [slide, setSlide] = useState({
-    translate: 0,
-    transition: 0.45,
-    activeIndex: 0,
-  });
+const PhotoContainer = styled.div`
+  position: relative;
+  display: flex;
+  height: 700px;
+  width: 700px;
+  transition: transform ease-out 0.3s;
+`;
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
 
-  const { translate, transition, activeIndex } = state;
+  img {
+    width: 700px;
+    height: 700px;
+    object-fit: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+`;
 
-  const getWidth = () => window.innerWidth;
+const ImageSlider = ({ img, i }) => {
+  const [index, setIndex] = useState(0);
+  const [translateX, setTranslateX] = useState(0);
+  const [images, setImages] = useState();
 
-  const prevSlide = () => {};
-  const nextSlide = () => {};
+  const accessKey =
+    "d5aeca2e0d9de8c9724b8ccffa6e1c7f0b9eefd73acc42ce1d64be3b421f8ad9";
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = () => {
+    axios
+      .get(
+        `https://api.unsplash.com/users/hakantas212/photos/?&per_page=50&client_id=${accessKey}`
+      )
+      .then((res) => {
+        setImages(res.data);
+      })
+      .catch((error) => {});
+  };
+
+  const handlePrev = () => {
+    if (index === 0) {
+      setTranslateX(translateX);
+    } else {
+      setIndex(index - 1);
+      setTranslateX(translateX + slideWidth());
+    }
+  };
+
+  const handleNext = () => {
+    if (index === fetchImages.length - 1) {
+      setIndex(0);
+      setTranslateX(0);
+    } else {
+      setIndex(index + 1);
+      setTranslateX(translateX + -slideWidth());
+    }
+  };
+
+  const slideWidth = () => {
+    console.log(document.querySelector(".slider-container").clientWidth);
+    return document.querySelector(".slider-container").clientWidth;
+  };
   return (
-    <Container>
-      <Images />
-      <PrevButton onClick={prevSlide}>PREV</PrevButton>
-      <NextButton onClick={nextSlide}>NEXT</NextButton>
+    <Container className="slider-container">
+      <PhotoContainer style={{ transform: `translateX(${translateX}px)` }}>
+        {" "}
+        {images &&
+          images.map((img, i) => (
+            <ImageWrapper className="image-wrapper" key={i}>
+              <img alt={img.alt_description} src={img.urls.regular} />
+            </ImageWrapper>
+          ))}
+      </PhotoContainer>{" "}
+      <PrevButton onClick={handlePrev}>PREV</PrevButton>
+      <NextButton onClick={handleNext}>NEXT</NextButton>
     </Container>
   );
 };
